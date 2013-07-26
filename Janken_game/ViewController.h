e74bba281cde74b389815967872098db53fb2bfa
ONOS <onoshidesu@gmail.com>
2013-07-26 11:47:10 +0900

    じゃんけんアプリ version 1.0

--- Janken_game/ViewController.h
+++ Janken_game/ViewController.h
@@ -7,7 +7,50 @@
 //
 
 #import <UIKit/UIKit.h>
+//数字をじゃんけんのキーとして登録
+#define Rock_Key 1
+#define Scissors_Key 2
+#define Paper_Key 3
+//UIに表示される日本語をまとめて登録
+#define Rematch_Button_Initial_Message @"もういっかい"
+#define Message_Label_Initial_Message @"じゃんけん・・・　　　　"
+#define Message_Label_Game_Message @"じゃんけん・・・ぽんっ！"
+#define Win_Message @"あなたの勝ち！"
+#define Lose_Message @"あなたの負け！"
+#define Draw_Message @"あいこで・・・"
+#define Error_Message @"不正な値が入力されました"
+//結果表示時の文字のカラー変更を登録
+#define Win_Color blueColor
+#define Lose_Color redColor
+#define Draw_Color blackColor
 
-@interface ViewController : UIViewController
 
+@interface ViewController : UIViewController {
+    IBOutlet UIButton *Rock_Button;
+    IBOutlet UIButton *Scissors_Button;
+    IBOutlet UIButton *Paper_Button;
+    IBOutlet UIButton *Rematch_Button;
+    IBOutlet UILabel *Message_Label;
+    IBOutlet UILabel *Result_Label;
+    IBOutlet UIImageView *Enemy_Select_Image;
+    
+    UIImage *Rock_Image;
+    UIImage *Scissors_Image;
+    UIImage *Paper_Image;
+}
+
+//じゃんけんのボタンを押したときに呼び出されるメソッド
+- (IBAction)Player_Select_Tactics:(id)sender;
+//相手の手の選択と表示を行うメソッド
+- (NSInteger) Enemy_Select_Tactics;
+//勝敗を判定するメソッド
+- (void) Decide_Win_or_Lose_Player:(NSInteger)Player_Tactics_Key _and_Enemy:(NSInteger)Enemy_Tactics_Key;
+//勝ち、負け、またはあいこの場合の動作を行うメソッド
+- (void) Win_View_Setting;
+- (void) Lose_View_Setting;
+- (void) Draw_View_Setting;
+//じゃんけんボタンの表示と有効化を行うメソッド
+- (void) Initial_View;
+//再戦ボタンの押したときに呼び出されるメソッド
+- (IBAction)Hope_Rematch:(id)sender;
 @end
--- Janken_game/ViewController.m
+++ Janken_game/ViewController.m
@@ -17,7 +17,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    Rock_Image = [UIImage imageNamed:@"gu.png"];
+    Scissors_Image = [UIImage imageNamed:@"ch.png"];
+    Paper_Image = [UIImage imageNamed:@"pa.png"];
+    
+    Rock_Button.tag = Rock_Key;
+    Scissors_Button.tag = Scissors_Key;
+    Paper_Button.tag = Paper_Key;
+    
+    Message_Label.text = Message_Label_Initial_Message;
+    Enemy_Select_Image.hidden = YES;
+    Result_Label.hidden = YES;
+    [Rematch_Button setTitle:Rematch_Button_Initial_Message forState:UIControlStateNormal];
+    Rematch_Button.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +39,148 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)Player_Select_Tactics:(id)sender {
+    Message_Label.text = Message_Label_Game_Message;
+    
+    UIButton *Player_Select_Button = (UIButton*) sender;
+    
+    switch (Player_Select_Button.tag) {
+        case Rock_Key:
+            [Rock_Button setEnabled:NO];
+            Scissors_Button.hidden = YES;
+            Paper_Button.hidden = YES;
+            break;
+        case Scissors_Key:
+            [Scissors_Button setEnabled:NO];
+            Rock_Button.hidden = YES;
+            Paper_Button.hidden = YES;
+            break;
+        case Paper_Key:
+            [Paper_Button setEnabled:NO];
+            Rock_Button.hidden = YES;
+            Scissors_Button.hidden = YES;
+            break;
+        default:
+            Message_Label.text = Error_Message;
+            break;
+    }
+    
+    NSInteger Enemy_Select_Button = [self Enemy_Select_Tactics];
+    
+    [self Decide_Win_or_Lose_Player:Player_Select_Button.tag _and_Enemy:Enemy_Select_Button];
+}
+
+-(NSInteger) Enemy_Select_Tactics {
+    Enemy_Select_Image.hidden = NO;
+    NSInteger Enemy_Select_Key = arc4random() % 3;
+    
+    switch (Enemy_Select_Key) {
+        case 0:
+            [Enemy_Select_Image setImage:Rock_Image];
+            return Rock_Key;
+        case 1:
+            [Enemy_Select_Image setImage:Scissors_Image];
+            return Scissors_Key;
+        case 2:
+            [Enemy_Select_Image setImage:Paper_Image];
+            return Paper_Key;
+        default:
+            Message_Label.text = Error_Message;
+            return 0;
+    }
+}
+
+- (void) Decide_Win_or_Lose_Player:(NSInteger)Player_Tactics_Key _and_Enemy:(NSInteger)Enemy_Tactics_Key {
+    Result_Label.hidden = NO;
+    
+    switch (Player_Tactics_Key) {
+        case Rock_Key:
+            switch (Enemy_Tactics_Key) {
+                case Rock_Key:
+                    [self Draw_View_Setting];
+                    break;
+                case Scissors_Key:
+                    [self Win_View_Setting];
+                    break;
+                case Paper_Key:
+                    [self Lose_View_Setting];
+                    break;
+                default:
+                    Message_Label.text = Error_Message;
+                    break;
+            }
+            break;
+        case Scissors_Key:
+            switch (Enemy_Tactics_Key) {
+                case Rock_Key:
+                    [self Lose_View_Setting];
+                    break;
+                case Scissors_Key:
+                    [self Draw_View_Setting];
+                    break;
+                case Paper_Key:
+                    [self Win_View_Setting];
+                    break;
+                default:
+                    Message_Label.text = Error_Message;
+                    break;
+            }
+            break;
+        case Paper_Key:
+            switch (Enemy_Tactics_Key) {
+                case Rock_Key:
+                    [self Win_View_Setting];
+                    break;
+                case Scissors_Key:
+                    [self Lose_View_Setting];
+                    break;
+                case Paper_Key:
+                    [self Draw_View_Setting];
+                    break;
+                default:
+                    Message_Label.text = Error_Message;
+                    break;
+            }
+            break;
+        default:
+            Message_Label.text = Error_Message;
+            break;
+    }
+}
+
+- (void) Win_View_Setting {
+    [Result_Label setTextColor:[UIColor Win_Color]];
+    Result_Label.text = Win_Message;
+    Rematch_Button.hidden = NO;
+}
+
+- (void) Lose_View_Setting {
+    [Result_Label setTextColor:[UIColor Lose_Color]];
+    Result_Label.text = Lose_Message;
+    Rematch_Button.hidden = NO;
+}
+
+- (void) Draw_View_Setting {
+    [Result_Label setTextColor:[UIColor Draw_Color]];
+    Result_Label.text = Draw_Message;
+    [self Initial_View];
+}
+
+- (void) Initial_View {
+    [Rock_Button setEnabled:YES];
+    [Scissors_Button setEnabled:YES];
+    [Paper_Button setEnabled:YES];
+    Rock_Button.hidden = NO;
+    Paper_Button.hidden = NO;
+    Scissors_Button.hidden = NO;
+    Rematch_Button.hidden = YES;
+}
+
+- (IBAction)Hope_Rematch:(id)sender {
+    Message_Label.text = Message_Label_Initial_Message;
+    [self Initial_View];
+    Enemy_Select_Image.hidden = YES;
+    [Enemy_Select_Image setImage:nil];
+    Result_Label.hidden = YES;
+}
 @end
